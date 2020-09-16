eea3b56cee5c822ef3c7e36ac9e6b5dd1d1a970c
sumini97 <jamie1541@naver.com>
2020-09-16 19:16:25 +0900

    layout design

--- assets/images.js
+++ assets/images.js
@@ -6,6 +6,11 @@ const images = {
     right: require('./img/right.png'),
     correct: require('./img/correct.png'),
     line: require('./img/line.png'),
+    TermButton: require('./img/TermButton.png'),
+    TermButton2: require('./img/TermButton2.png'),
+    check: require('./img/check.png'),
+    check2: require('./img/check2.png'),
+    point: require('./img/point.png')
 };
 
 export default images;
